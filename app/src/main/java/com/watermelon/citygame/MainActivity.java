0c1cd632caac697f16d8b5c74016dc073352ab18
kblc9l <leoleonkis3@gmail.com>
2022-01-09 15:26:53 +0400

    проверка
    написал проверку на пустую строку

--- app/src/main/java/com/watermelon/citygame/MainActivity.java
+++ app/src/main/java/com/watermelon/citygame/MainActivity.java
@@ -2,10 +2,14 @@ package com.watermelon.citygame;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    Button bt;
 
 
     @Override
@@ -14,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.city_field_auto);
         textView.setText("Тюмень");
+        bt = findViewById(R.id.button);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = (String) textView.getText();
+                if(str.length() == 0)
+                    Toast.makeText(MainActivity.this, "ты ничего не написал", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
