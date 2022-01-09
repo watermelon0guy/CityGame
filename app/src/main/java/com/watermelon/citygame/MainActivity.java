323d5a5e342628acb48fe270aeb6bb51886c1a49
watermelon0guy <watermelon0guy@users.noreply.github.com>
2022-01-09 15:49:30 +0300

    Парсинг файла

--- app/src/main/java/com/watermelon/citygame/MainActivity.java
+++ app/src/main/java/com/watermelon/citygame/MainActivity.java
@@ -1,12 +1,26 @@
 package com.watermelon.citygame;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
     TextView cityFieldAuto;
@@ -21,6 +35,19 @@ public class MainActivity extends AppCompatActivity {
         bt = findViewById(R.id.button);
         cityFieldAuto = findViewById(R.id.city_field_auto);
         cityField = findViewById(R.id.city_field);
+        ArrayList<String> cities = new ArrayList<>();
+        String fileStr = "";
+        try {
+            Resources res = getResources();
+            InputStream inputStream = res.openRawResource(R.raw.city);
+            Scanner s = new Scanner(inputStream);
+            while (s.hasNext())
+                cities.add(s.next());
+            System.out.println(fileStr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
