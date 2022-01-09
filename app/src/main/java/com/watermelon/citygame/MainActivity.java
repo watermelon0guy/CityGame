ea197134106f661194eb77270f16b2a0ef68b938
watermelon0guy <watermelon0guy@users.noreply.github.com>
2022-01-09 16:22:56 +0300

    Update MainActivity.java

--- app/src/main/java/com/watermelon/citygame/MainActivity.java
+++ app/src/main/java/com/watermelon/citygame/MainActivity.java
@@ -19,7 +19,10 @@ import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,27 +38,45 @@ public class MainActivity extends AppCompatActivity {
         bt = findViewById(R.id.button);
         cityFieldAuto = findViewById(R.id.city_field_auto);
         cityField = findViewById(R.id.city_field);
-        ArrayList<String> cities = new ArrayList<>();
-        String fileStr = "";
-        try {
-            Resources res = getResources();
-            InputStream inputStream = res.openRawResource(R.raw.city);
-            Scanner s = new Scanner(inputStream);
-            while (s.hasNext())
-                cities.add(s.next());
-            System.out.println(fileStr);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        ArrayList<String> cities_original = fileToArray(R.raw.city);
+        ArrayList<String> cities_current = new ArrayList<>(cities_original.size());
 
+        Collections.copy(cities_original, cities_current);
+        
+        int index = Math.abs((int)(Math.random() * cities_original.size() - 1));
+
+        cityFieldAuto.setText(cities_original.get(index));
 
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String str = cityField.getText().toString();
+                String autoCity = cityFieldAuto.getText().toString();
                 if(str.length() == 0)
-                    Toast.makeText(MainActivity.this, "ты ничего не написал", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Ты ничего не написал", Toast.LENGTH_SHORT).show();
+                else if (str.toLowerCase().charAt(0) != autoCity.charAt(autoCity.length()-1))
+                {
+                    Toast.makeText(MainActivity.this, "Слово не подходит", Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                }
             }
         });
+    }
+
+    ArrayList<String> fileToArray(int file)
+    {
+        ArrayList<String> cities = new ArrayList<>();
+        try {
+            Resources res = getResources();
+            InputStream inputStream = res.openRawResource(file);
+            Scanner s = new Scanner(inputStream);
+            while (s.hasNext())
+                cities.add(s.next());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cities;
     }
 }
